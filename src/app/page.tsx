6133941fc132e1f592221b69a83a714b4bdc87b3
HasanAkhonda <mdmahmudhasanakhonda@gmail.com>
2025-05-03 08:43:18 +0600

    5th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
       <HeroParallax/>
       <MarketingServices/>
       <MarketingFeatures/>
+      <MarketingFeatures/>
       <ScrollPinHero/>
     </div>
   );
